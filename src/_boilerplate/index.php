79c014ba31a1eb97fbad619eaff1d4f7587c8074
Morgan Wigmanich <okize123@gmail.com>
2013-01-24 17:58:25 -0500

    updates to boilerplate

---
+++ src/_boilerplate/index.php
@@ -0,0 +1,16 @@
+<?php error_reporting(E_ALL); ini_set('display_errors', '1'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>@edit</title>
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="http://loc.techtarget.com/ux/javascript/jquery-1.7.2.min.js"><\/script>')</script>
+</head>
+<body id="@edit">
+
+<?php include('main.php'); ?>
+<?php //include('abstract.php'); ?>
+
+</body>
+</html>
---
+++ src/_boilerplate/javascript/script.js
@@ -0,0 +1,9 @@
+/* 2013-01-28 */
+
+// BEGIN: doc ready
+$(document).on('ready', function ($) {
+
+  'use strict';
+
+});
+// END: doc ready
