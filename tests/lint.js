fde624652f79bc08a4eb1b8d97db94f2667d10da
Morgan Wigmanich <okize123@gmail.com>
2013-02-22 14:55:12 -0500

    split files.js into ftp & build

--- tests/lint.js
+++ tests/lint.js
@@ -27,26 +27,32 @@ var files = getJsFiles();
 
 var lint = function (data, filename) {
 
-    if ( jshint( data.toString() ) ) {
+  if ( jshint( data.toString() ) ) {
 
-      console.log('  ' + filename + ' has no errors.  \033[32m✔\033[39m');
+    console.log('    \033[32m✓\033[39m ' + filename + ' has no errors.');
 
-    } else {
+  } else {
 
-      console.log('Errors in file ' + filename);
-      var out = jshint.data(),
-      errors = out.errors;
+    console.log('Errors in file ' + filename);
+    var out = jshint.data(),
+    errors = out.errors;
 
-      for(var j=0;j<errors.length;j++) {
-        console.log(errors[j].line + ':' + errors[j].character + ' -> ' + errors[j].reason + ' -> ' + errors[j].evidence);
-      }
-
+    for(var j=0;j<errors.length;j++) {
+      console.log(errors[j].line + ':' + errors[j].character + ' -> ' + errors[j].reason + ' -> ' + errors[j].evidence);
     }
 
+  }
+
 };
+
+// iterrate javascript files though linter and record output to console
+
+console.log('Linting javascript... \n');
 
 for (var i = 0, len = files.length; i < len; i++) {
 
   lint( fs.readFileSync(files[i], 'utf8'), files[i] );
 
 }
+
+console.log('\n');
